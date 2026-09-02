550f3ed0aca70518497377e170c26bf91622114a
Just a baka <416302+justabaka@users.noreply.github.com>
2026-09-02 22:10:32 +0300

    Delete lock file on start if mods are enabled
    It is the only way to circumvent the lack of 'ficsit update' feature at the moment: https://github.com/satisfactorymodding/ficsit-cli/issues/79#issuecomment-3304733124

--- install-mods.sh
+++ install-mods.sh
@@ -2,11 +2,12 @@
 set -euo pipefail
 
 ENABLE_MODS=${ENABLE_MODS:-0}
-echo -e "Mod manager script is now running. Mods are set to be\033[1m $([[ $ENABLE_MODS -eq 1 ]] && echo -e "\033[38;5;10m*ENABLED*" || echo -e "\033[38;5;196m*DISABLED*") \033[0mby the ENABLE_MODS server variable."
+echo -e "Mod manager script is now running. Mods are set to be\033[1m $([[ ${ENABLE_MODS} -eq 1 ]] && echo -e "\033[38;5;10m*ENABLED*" || echo -e "\033[38;5;196m*DISABLED*") \033[0mby the ENABLE_MODS server variable."
 
 GAME_DIR="/home/container"
-FICSIT_DIR="$GAME_DIR/Ficsit"
-FICSIT_FLAGS="--local-dir $GAME_DIR/Ficsit --cache-dir $GAME_DIR/Ficsit/cache"
+FICSIT_DIR="${GAME_DIR}/Ficsit"
+FICSIT_FLAGS="--local-dir ${GAME_DIR}/Ficsit --cache-dir $GAME_DIR/Ficsit/cache"
+FICSIT_LOCK_FILE="${GAME_DIR}/FactoryGame/Mods/default-lock.json"
 
 echo -e "\nDownloading ficsit-cli..."
 mkdir -p ${FICSIT_DIR}
@@ -22,8 +23,13 @@ else
 fi
 
 if [[ "${ENABLE_MODS}" -eq 1 ]]; then
-	echo -e "\nConverting SMM mods profile into ficsit-cli format..."
+	if [[ -f "${FICSIT_LOCK_FILE}" ]]; then
+		echo -e "\nRemoving old ficsit lock file '${FICSIT_LOCK_FILE}' if present..."
+		rm "${FICSIT_LOCK_FILE}"
+	fi
+	
 	if [[ -f "${FICSIT_DIR}/smm.json" ]]; then
+		echo -e "\nConverting SMM mods profile into ficsit-cli format..."
 		jq '{
   profiles: {
     Default: {
@@ -35,6 +41,7 @@ if [[ "${ENABLE_MODS}" -eq 1 ]]; then
   selected_profile: "Default",
   version: 0
 }' ${FICSIT_DIR}/smm.json > ${FICSIT_DIR}/profiles.json
+
 		rm ${FICSIT_DIR}/smm.json
 		echo "Mod profile has been successfully imported from '${FICSIT_DIR}/smm.json', deleting the source file..."
 		echo "Only reupload '${FICSIT_DIR}/smm.json' if you actually changed something (e.g. added/removed a mod)."
